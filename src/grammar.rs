428c0f6f5f2a5aba24a0ff70bfad7b62cdc9d962
grandroad <3144027763@qq.com>
2021-06-09 22:48:06 +0800

    实现将存放在Grammar.rule文件中的文法读入并初始化Grammar数据结构

--- src/grammar.rs
+++ src/grammar.rs
@@ -1,10 +1,36 @@
-using std::collections::HashMap;
-
+use std::collections::HashMap;
+use std::fs;
 
 enum Symbol {
     VoidSymbol,
+    EndSymbol,
     Terminate(char),
     NonTerminate(char)
+}
+
+impl Symbol {
+    fn from(c: char) -> Symbol {
+        if c <= 'Z' && c >= 'A' {
+            Symbol::NonTerminate(c)
+        }
+        else if c == '$' {
+            Symbol::VoidSymbol
+        }
+        else if c == '#' {
+            Symbol::EndSymbol
+        }
+        else {
+            Symbol::Terminate(c)
+        }
+    }
+    fn to_char(&self) -> char {
+        match self {
+            Symbol::VoidSymbol => '$',
+            Symbol::EndSymbol => '#',
+            Symbol::Terminate(c) => *c,
+            Symbol::NonTerminate(c) => *c
+        }
+    }
 }
 
 pub struct Production {
@@ -31,6 +57,68 @@ impl Grammar {
         Grammar {
             symbol_table: Vec::new(),
             productions: Vec::new()
+        }
+    }
+    fn symbol_is_exist(&self, c: char) -> bool {
+        for elem in &self.symbol_table {
+            if c == elem.to_char() {
+                return true;
+            }
+        }
+        false
+    }
+    fn get_char_index(&self, c: char) -> usize {
+        let mut index: usize = 0;
+        for elem in &self.symbol_table {
+            if c == elem.to_char() {
+                return index;
+            }
+            index = index + 1;
+        };
+        0
+    }
+    pub fn init_grammar(&mut self, filename: &str) {
+        let file_content = match fs::read_to_string(filename) {
+            Ok(f_c) => f_c,
+            _ => String::from("there is a error occur when init.")
+        };
+        self.symbol_table.push(Symbol::from('@'));
+        self.productions.push(Production::new(0, vec![1]));
+        for s in file_content.lines() {
+            let head;
+            let mut tail: Vec<usize> = Vec::new();
+            let head_tail: Vec<&str> = s.split(' ').collect();
+            let head_char = head_tail[0].as_bytes()[0] as char ;
+            if !self.symbol_is_exist(head_char) {
+                self.symbol_table.push(Symbol::from(head_char));
+            }
+            head = self.get_char_index(head_char);
+
+            for c in head_tail[1].chars() {
+                
+                if !self.symbol_is_exist(c) {
+                    self.symbol_table.push(Symbol::from(c));
+                }
+                tail.push(self.get_char_index(c));
+            }
+
+            self.productions.push(Production::new(head, tail));
+            println!("{}", s);
+        }
+    }
+    pub fn print_grammar(&self) {
+        print!("symbol table:");
+        for elem in &self.symbol_table {
+            print!("{} ", elem.to_char());
+        }
+        println!();
+        println!("productions:");
+        for elem in &self.productions {
+            print!("{}->", self.symbol_table[elem.head].to_char());
+            for i in &elem.tail {
+                print!("{}", self.symbol_table[*i].to_char());
+            }
+            println!();
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-
+mod grammar;
+use grammar::*;
 
 fn main() {
+    let mut gm = Grammar::new();
+    gm.init_grammar("Grammar.rule");
     println!("Hello, world!");
+    gm.print_grammar();
 }
